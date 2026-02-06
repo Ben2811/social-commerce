29c89ca4dfe2ea8b067123ab182600d2a2d37008
Your Name <mthu131204@gmail.com>
2026-02-07 01:41:28 +0700

    Update category page UI

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { categories } from "../data/categories";
 
 function CategoryPage() {
+    const [selected, setSelected] = useState(null);
+
   return (
     <div>
       <h2>Danh mục sản phẩm</h2>
 
       <ul>
         {categories.map((c) => (
-          <li key={c.id}>{c.name}</li>
+          <li key={c.id}
+            onClick={() => setSelected(c.name)}
+            style={{ cursor: "pointer" }}
+            >{c.name}
+            </li>
         ))}
       </ul>
+      {selected && <p>Đang xem danh mục: {selected}</p>}
     </div>
   );
 }
